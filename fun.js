cd58cf8829bed0bd114e5f17dad45ad4372beed1
Abilash Badri <abadri@paypal.com>
2014-02-04 10:42:46 -0800

    Session 1

---
+++ fun.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+	return x+y;
+}
+
+function sub(x, y) {
+	return x-y;
+}
+
+function mul(x, y) {
+	return x*y;
+}
+
+
+
+console.log(add (3, 4));
+console.log(sub (3, 4));
+console.log(mul (3, 4));
