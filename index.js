9eae343c12561ded17c5cacf248c2c5ea9dbb5f8
Abilash Badri <abadri@paypal.com>
2014-02-04 11:20:22 -0800

    Update to JS

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mul(3, 4);
 function identityf(x) {
     return function () {
         return x;
-    }
+    };
 }
 
 var idf3 = identityf(3);
@@ -32,7 +32,7 @@ idf3();
 function addf(x) {
     return function (y) {
         return x + y;
-    }
+    };
 }
 
 addf(3)(4);
@@ -67,7 +67,7 @@ applyf(mul)(5)(6);
 function curry(fun, x) {
     return function (y) {
         return fun(x, y);
-    }
+    };
 }
 
 var add3 = curry(add, 3);
@@ -81,11 +81,11 @@ curry(mul, 5)(6); //30
 
 var inc = function (x) {
     return add(1, x);
-}
+};
 
 var inc2 = function (x) {
     return applyf(add)(1)(x);
-}
+};
 
 var inc3 = curry(add, 1);
 
@@ -104,7 +104,7 @@ inc3(inc2(5));
 function twice(fun) {
     return function (x) {
         return fun(x, x);
-    }
+    };
 }
 
 add(11, 11); //22
@@ -114,15 +114,45 @@ doubl(2);
 square(11); //121
 
 
-function switcheroo(fun) {
-    return function (x, y) {
-        return fun(y, x);
-    }
-}
-
 /*
  * Write a switcheroo function that reverses the arguments of a
  * binary function.
  */
+
+function switcheroo(fun) {
+    return function (x, y) {
+        return fun(y, x);
+    };
+}
+
 var bus = switcheroo(sub);
-console.log(bus(3, 2)); //-1
+bus(3, 2); //-1
+
+/*
+  * Write a function composeu that takes two unary functions
+  * and returns a unary function that calls them both
+  * composeu(doubl, square)(3); //36
+  * 36 = (3+3) * (3+3)
+ */
+
+function composeu(fun1, fun2) {
+    return function (x) {
+        return fun2(fun1(x));
+    };
+}
+
+composeu(doubl, square)(3); //36
+
+/*
+ * Write a function composeb that takes two binary functions and returns
+ * a function that calls them both
+ * e.g. composeb(add, mul) (2, 3, 5); //25
+ */
+
+function composeb(fun1, fun2) {
+    return function (x, y, z) {
+        return fun2(fun1(x,y), z);
+    };
+}
+
+console.log(composeb(add, mul) (2, 3, 5)); //25
